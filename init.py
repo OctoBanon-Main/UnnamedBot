e946005514808985541f3b12cf0b1f22e0de9276
OctoBanon <82901151+OctoBanon-Main@users.noreply.github.com>
2021-08-22 05:01:17 +0700

    Update init.py

--- init.py
+++ init.py
@@ -3,5 +3,4 @@ import sqlite3
 def startup():
     con = sqlite3.connect("database.db")
     cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS report_channel (channel_id INTEGER, guild_id INTEGER)")
     cur.execute("CREATE TABLE IF NOT EXISTS warns (user_id INTEGER, guild_id INTEGER, warns INTEGER)")
