d05a5e5b2d5e4f243a7f910b6a30fc54636a3b14
OctoBanon <82901151+OctoBanon-Main@users.noreply.github.com>
2021-10-10 02:46:16 +0700

    Delete init.py

--- init.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-def startup():
-    con = sqlite3.connect("database.db")
-    cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS guild_warnings (member_id INTEGER, guild_id INTEGER, warnings INTEGER)")
